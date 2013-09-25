a2cdda3273d719d2c04c72320a4088e50ffe5abb
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2013-09-25 18:10:12 +0200

    web: add spring mvc validation exception handler

--- springhub-web/src/main/java/com/mjeanroy/springhub/controllers/AbstractController.java
+++ springhub-web/src/main/java/com/mjeanroy/springhub/controllers/AbstractController.java
@@ -1,5 +1,16 @@
 package com.mjeanroy.springhub.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import com.mjeanroy.springhub.commons.web.utils.Browser;
 import com.mjeanroy.springhub.exceptions.DisconnectedException;
 import com.mjeanroy.springhub.exceptions.EntityNotFoundException;
@@ -7,20 +18,13 @@ import com.mjeanroy.springhub.exceptions.NotImplementedException;
 import com.mjeanroy.springhub.exceptions.RequestParameterException;
 import com.mjeanroy.springhub.exceptions.ResourceNotFoundException;
 import com.mjeanroy.springhub.exceptions.UnauthorizedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public abstract class AbstractController {
 
-	/** Class logger */
+	/**
+	 * Class logger
+	 */
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractController.class);
 
 	@Autowired
@@ -105,6 +109,12 @@ public abstract class AbstractController {
 	public void resourceNotFoundException(ResourceNotFoundException ex, HttpServletResponse response) {
 		LOG.error(ex.getMessage());
 		setResponse(response, 404, ex.getMessage());
+	}
+
+	@ExceptionHandler(value = {MethodArgumentNotValidException.class})
+	public void methodArgumentNotValid(MethodArgumentNotValidException exception, HttpServletResponse response) throws IOException {
+		MethodArgumentNotValidException e = (MethodArgumentNotValidException) exception;
+		setResponse(response, 400, exception.getMessage());
 	}
 
 	@ExceptionHandler(Exception.class)
