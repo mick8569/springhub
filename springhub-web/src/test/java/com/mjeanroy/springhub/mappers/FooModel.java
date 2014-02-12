6e3ff6fc38a9a9f8013bab13a3a669b3ef1e58fe
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-02-12 17:00:44 +0100

    fix: add log when brower data are not available

--- springhub-web/src/test/java/com/mjeanroy/springhub/mappers/FooModel.java
+++ springhub-web/src/test/java/com/mjeanroy/springhub/mappers/FooModel.java
@@ -1,22 +1,19 @@
 package com.mjeanroy.springhub.mappers;
 
-import com.mjeanroy.springhub.models.AbstractModel;
+import com.mjeanroy.springhub.models.entities.AbstractEntity;
 
-public class FooModel extends AbstractModel {
+public class FooModel extends AbstractEntity {
 
 	private Long id;
 
 	private String name;
 
 	@Override
-	public Long modelId() {
-		return getId();
-	}
-
 	public Long getId() {
 		return id;
 	}
 
+	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}
