20dbcd810028bad3d8cabbedd71a952b2a2b7299
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-08-10 23:53:31 +0200

    feat(web): add entity manager in view annotation

--- springhub-web/src/main/java/com/mjeanroy/springhub/configuration/EntityManagerInterceptorConfiguration.java
+++ springhub-web/src/main/java/com/mjeanroy/springhub/configuration/EntityManagerInterceptorConfiguration.java
@@ -1,11 +1,24 @@
 package com.mjeanroy.springhub.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
 
+@Slf4j
 @Configuration
-public class EntityManagerInterceptorConfiguration {
+public class EntityManagerInterceptorConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		log.info("Add entity manager in view");
+		OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor = openEntityManagerInViewInterceptor();
+		WebRequestHandlerInterceptorAdapter adapter = new WebRequestHandlerInterceptorAdapter(openEntityManagerInViewInterceptor);
+		registry.addInterceptor(adapter);
+	}
 
 	@Bean
 	public OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor() {
